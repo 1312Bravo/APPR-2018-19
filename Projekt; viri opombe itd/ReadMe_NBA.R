68e1ed220805d9df580a498b6b6ae4fe6906b52a
Urh Peček <pecek.urh@gmail.com>
2018-11-21 19:14:12 +0100

    Zasnova README

---
+++ Projekt; viri opombe itd/ReadMe_NBA.R
@@ -0,0 +1,34 @@
+## Projekt pri Predmetu APPR v šolskem letu 2018/19
+
+## Naslov
+
+Igralci in države v ligi NBA
+
+## Avtor
+
+Urh Peček
+
+## Osnovna ideja
+
+- Primerjava igralcev v ligi NBA glede na nacionalnost:
+  - Statistični indeksi
+  - dvig/padev forme
+  - plača
+  - poškodbe
+  - pregled sezon (16,17) 18, predikcija za 2019 (tekoča)
+
+## Glavni viri: 
+- https://www.basketball-reference.com/
+- http://www.espn.com/nba/statistics
+- https://stats.nba.com/
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
